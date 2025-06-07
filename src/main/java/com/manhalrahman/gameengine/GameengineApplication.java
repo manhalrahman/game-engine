efc3ac76fc1fe9786c1014e17425a38d14b4860d
manhalrahman <mrmanhal99@gmail.com>
2025-06-07 12:22:16 +0530

    Intial setup - Spring Boot 3.5

---
+++ src/main/java/com/manhalrahman/gameengine/GameengineApplication.java
@@ -0,0 +1,13 @@
+package com.manhalrahman.gameengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manhalrahman/gameengine/GameengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhalrahman.gameengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
